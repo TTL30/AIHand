ef451946d2e5c638b706681ee0168a69c5461c32
TTL30 <47627688+TTL30@users.noreply.github.com>
2020-11-18 17:14:04 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import numpy as np
 import cv2
-from time import sleep
+import time 
 from scipy import ndimage
 
 from skimage import io, transform
+from skimage.util import random_noise
+import random
 
 capture = cv2.VideoCapture(0)
 x = 200
@@ -12,16 +14,27 @@ w = 200
 h = 200
 var = 0
 
-#from keras import models, layers
-#import tensorflow as tf
-#model = models.load_model('model')
+""" from keras import models, layers
+import tensorflow as tf
+model = models.load_model('model') """
 
+def capt_data(thresh):
+    steps = [120,120,120,10,10,10,60,60,10,10,10,10,10,10,0]
+    n = 0
+    next = time.time() + steps[0]
+    title = "_Digit_4"
+    now = time.time()
+    if now >= next:
+        n += 1
+        next = now + steps[n]
+        cv2.imwrite('dataset/'+title, thresh)
+    time.sleep(1)
 
 
 def center_image(img, frame):
     global var
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret,thresh = cv2.threshold(gray_image,200,255,0)
+    ret,thresh = cv2.threshold(gray_image,190,255,0)
     M = cv2.moments(thresh)
     if(M["m00"] != 0):
         cX = int(M["m10"] / M["m00"])
@@ -44,21 +57,28 @@ def center_image(img, frame):
         roi = frame[pos_y:pos_y+h,pos_x:pos_x+w]
         img_grey_center = transform.resize(roi, (128,128,1), mode = 'constant')
         
-        ret2,thresh2 = cv2.threshold(img_grey_center,127,255,0)
+        ret2,thresh2 = cv2.threshold(img_grey_center,190,255,0)
 
-        if var == 0:
-            print(img_grey_center.shape)
-            var = 1
+        
         cv2.imshow("Threshold", thresh)
         cv2.imshow("Roi recentered", img_grey_center)
-        #cv2.imshow("reeezaes", img_read)
+        gray_image2 = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        ret2,thresh2 = cv2.threshold(gray_image2,200,255,0)
+        cv2.imshow("nois", thresh2)
+        if var == 0:
+            capt_data(thresh2)
+            var = 1
+      
+
         
-        """ data = data/255.0
-        pred = model.predict(img_read)[0]
+        """  data = img_grey_center_2/255.0
+        data = np.expand_dims(data, axis=0)
+        pred = model.predict(data)[0]
         pred2 = pred[:4]
         print(" is : ", np.argmax(pred))
-        """
+         """
  
+
 while(True):
     ret, frame = capture.read()
     cv2.imshow('frame', frame)
