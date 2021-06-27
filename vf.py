b781a2673ba14c169263f33400587e59abe62057
ttl30 <tiagoteixeira3007@gmail.com>
2021-06-27 18:56:36 +0200

    end

--- vf.py
+++ vf.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from keras.models import model_from_json
 from collections import Counter
+import warnings
 
 cap = cv2.VideoCapture(0)
 
@@ -14,14 +17,16 @@ y = 0
 w = 200
 h = 200
 model = 0
+on = False
 
 def nothing(x):
+    print("[INFO]New Threshold value : ",x)
     pass
 
 file1 = open("new.txt","r+")  
 v = int(file1.read())
 
-cv2.createTrackbar("R", "threshold", v, 255, nothing)
+cv2.createTrackbar("Value", "threshold", v, 255, nothing)
 
 def load_model():
     print("[INFO] Loading model...")
@@ -67,13 +72,16 @@ op = False
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 last = False
-
-out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+cX = 0
+cY = 0
 data_arr =[]
+print("[INFO]INITIALISATION...")
+print("[INFO]Threshold value : ",v)
+
 while True:
     _, frame = cap.read()
+    r = cv2.getTrackbarPos("Value", "threshold")
 
-    r = cv2.getTrackbarPos("R", "threshold")
     with open ('new.txt','w') as f:
              f.write(str(r))
 
@@ -87,47 +95,55 @@ while True:
     mask = cv2.dilate(mask, None, iterations=2)
     contours2,_ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     center = None
-    if len(contours2) > 0:
+    if (len(contours2) > 0):
         c = max(contours2, key=cv2.contourArea)
         rect = cv2.minAreaRect(c)
         M = cv2.moments(c)
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
         cv2.circle(frame, center, 5, (0, 255, 0), -1)
-        if((rect[0][0]>=50 and rect[0][0]<=150) and (rect[0][1]>=220 and rect[0][1]<=270)):
-            if(op):
-                if(last):
-                    eq = eq[:-1]
-                else:
-                    last = True
-                eq += "+"
-        if((rect[0][0]>=50 and rect[0][0]<=150) and (rect[0][1]>=290 and rect[0][1]<=340)):
-            if(op):
-                if(last):
-                    eq = eq[:-1]
-                else:
-                    last = True 
-                eq += "-"
-        if((rect[0][0]>=50 and rect[0][0]<=150) and (rect[0][1]>=360 and rect[0][1]<=410)):
-            if(op):
-                if(last):
-                    eq = eq[:-1]
-                else:
-                    last = True
-                eq += "*"
+        if((rect[0][0]>=520 and rect[0][0]<=620) and (rect[0][1]>=220 and rect[0][1]<=270) and on == False):
+                print("[INFO] Calculatrice ON...")
+                on = True
+        if(on):
+            if((rect[0][0]>=20 and rect[0][0]<=120) and (rect[0][1]>=220 and rect[0][1]<=270)):
+                if(op):
+                    if(last):
+                        eq = eq[:-1]
+                    else:
+                        last = True
+                    eq += "+"
 
-        if((rect[0][0]>=450 and rect[0][0]<=550) and (rect[0][1]>=360 and rect[0][1]<=410)):
-            if(op):
+            if((rect[0][0]>=20 and rect[0][0]<=120) and (rect[0][1]>=290 and rect[0][1]<=340)):
+                if(op):
+                    if(last):
+                        eq = eq[:-1]
+                    else:
+                        last = True 
+                    eq += "-"
+
+            if((rect[0][0]>=20 and rect[0][0]<=120) and (rect[0][1]>=360 and rect[0][1]<=410)):
+                if(op):
+                    if(last):
+                        eq = eq[:-1]
+                    else:
+                        last = True
+                    eq += "*"
+
+            if((rect[0][0]>=520 and rect[0][0]<=620) and (rect[0][1]>=360 and rect[0][1]<=410)):
+                if(op):
+                    if(len(eq)>11):
+                        str_of_ints = "".join(eq[11:])
+                        res = eval(str_of_ints)
+                        eq2 = " = " + str(res)
+                        op = False
+
+            if((rect[0][0]>=520 and rect[0][0]<=620) and (rect[0][1]>=290 and rect[0][1]<=340)):
                 if(len(eq)>11):
-                    str_of_ints = "".join(eq[11:])
-                    res = eval(str_of_ints)
-                    eq2 = " = " + str(res)
                     eq = "Equation : "
-                    op = False
+                    eq2 = ""
+                    print("[INFO] Reset...")
+            
 
-        if((rect[0][0]>=450 and rect[0][0]<=550) and (rect[0][1]>=290 and rect[0][1]<=340)):
-            if(len(eq)>11):
-                eq = eq[:-1]
-                
 
     
     cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),1)
@@ -137,6 +153,7 @@ while True:
 
     ret,thresh = cv2.threshold(gray_image,r,255,4)
     ret2,thresh2 = cv2.threshold(gray_image,r,255,0)
+
 
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -148,32 +165,43 @@ while True:
     else:
         cv2.putText(frame, "Predicted : " + str(data), (10, 25) , cv2.FONT_HERSHEY_SIMPLEX,  
                    0.7, (0, 0, 255) , 1, cv2.LINE_AA)
+
     cv2.drawContours(cpy, [big_contour], -1, (0,0,0), -1)
     
-    cv2.rectangle(frame, (50, 420), (550, 470), (255, 255, 255), -1)
-    cv2.putText(frame, eq, (60, 455) , cv2.FONT_HERSHEY_SIMPLEX,  
+    cv2.rectangle(frame, (20, 420), (620, 470), (255, 255, 255), -1)
+    cv2.putText(frame, eq, (40, 455) , cv2.FONT_HERSHEY_SIMPLEX,  
                    1, (255, 0, 0) , 2, cv2.LINE_AA)
     cv2.putText(frame, eq2, (460, 455) , cv2.FONT_HERSHEY_SIMPLEX,  
                    1, (255, 0, 0) , 2, cv2.LINE_AA)
 
-    cv2.rectangle(frame, (50, 220), (150, 270), (255, 255, 255), -1)
-    cv2.putText(frame, "+", (90, 255) , cv2.FONT_HERSHEY_SIMPLEX,  
+    #Button +
+    cv2.rectangle(frame, (20, 220), (120, 270), (255, 255, 255), -1)
+    cv2.putText(frame, "+", (60, 255) , cv2.FONT_HERSHEY_SIMPLEX,  
                    1, (255, 0, 0) , 2, cv2.LINE_AA)
     
-    cv2.rectangle(frame, (50, 290), (150, 340), (255, 255, 255), -1)
-    cv2.putText(frame, "-", (90, 315) , cv2.FONT_HERSHEY_SIMPLEX,  
+    #Button -
+    cv2.rectangle(frame, (20, 290), (120, 340), (255, 255, 255), -1)
+    cv2.putText(frame, "-", (59, 325) , cv2.FONT_HERSHEY_SIMPLEX,  
                    1, (255, 0, 0) , 2, cv2.LINE_AA)
 
-    cv2.rectangle(frame, (50, 360), (150, 410), (255, 255, 255), -1)
-    cv2.putText(frame, "x", (90, 385) , cv2.FONT_HERSHEY_SIMPLEX,  
-                   1, (255, 0, 0) , 2, cv2.LINE_AA)
-    cv2.rectangle(frame, (450, 360), (550, 410), (255, 255, 255), -1)
 
-    cv2.putText(frame, "=", (490, 385) , cv2.FONT_HERSHEY_SIMPLEX,  
+    #Button X
+    cv2.rectangle(frame, (20, 360), (120, 410), (255, 255, 255), -1)
+    cv2.putText(frame, "x", (62, 391) , cv2.FONT_HERSHEY_SIMPLEX,  
                    1, (255, 0, 0) , 2, cv2.LINE_AA)
 
-    cv2.rectangle(frame, (450, 290), (550, 340), (255, 255, 255), -1)
-    cv2.putText(frame, "<=", (490, 315) , cv2.FONT_HERSHEY_SIMPLEX,  
+    #Button =
+    cv2.rectangle(frame, (520, 360), (620, 410), (255, 255, 255), -1)
+    cv2.putText(frame, "=", (557, 395) , cv2.FONT_HERSHEY_SIMPLEX,  
+                   1, (255, 0, 0) , 2, cv2.LINE_AA)
+
+    #Button AC
+    cv2.rectangle(frame, (520, 290), (620, 340), (255, 255, 255), -1)
+    cv2.putText(frame, "AC", (552, 325) , cv2.FONT_HERSHEY_SIMPLEX,  
+                   1, (255, 0, 0) , 2, cv2.LINE_AA)
+    
+    cv2.rectangle(frame, (520, 220), (620, 270), (255, 255, 255), -1)
+    cv2.putText(frame, "ON", (550, 255) , cv2.FONT_HERSHEY_SIMPLEX,  
                    1, (255, 0, 0) , 2, cv2.LINE_AA)
 
 
@@ -181,24 +209,27 @@ while True:
     if( M["m00"] != 0):
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
-        
+    if(big_contour.shape[0]>50):
         drawCenterMass(30) 
 
     cv2.drawContours(frame, [big_contour], -1, (0,255,0), 1)
-    out.write(frame)
 
     cv2.imshow("frame", frame)
     cv2.imshow("threshold", thresh)
     cv2.imshow("Background Removed", cpy)
+    cv2.imshow("hsv", hsv)
+    cv2.imshow("hsv mask", mask)
 
     if(model and big_contour.shape[0]>50):
         data = make_prediction(cpy,frame,model)
-        if(len(data_arr)<30):
+        if(len(data_arr)<25):
             data_arr.append(data)
         else:
             p = Counter(data_arr)
-            if(p.most_common(1)[0][1] > 20):
+            if(p.most_common(1)[0][1] > 20 and on):
+                print("[INFO] Prediction validé : ",p.most_common(1)[0][0])
                 eq +=  str(p.most_common(1)[0][0])
+                print("[INFO]",eq)
                 op = True
                 last = False
             data_arr.clear()
